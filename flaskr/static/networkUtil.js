c9778aad0bce44689a8390539463bb3801f75f34
mabby <rznns.mabby@gmail.com>
2024-04-13 22:21:05 -0600

    add & remove host

--- flaskr/static/networkUtil.js
+++ flaskr/static/networkUtil.js
@@ -28,18 +28,17 @@ function showConsole(event){
 }
 function addHost(event) {
     host = event.target.id
-    console.log('addHost: ', host)
-    // id = 'consola_'+host
-
+    id = host.split('_')[1]
+    
     // se crea elemento consola
     const contenedor = document.createElement('div');
-    contenedor.setAttribute("id", 'consola_'+host)
+    contenedor.setAttribute("id", 'consola_'+id)
     contenedor.classList.add('consola');
-
+    
     const branchHeader = document.createElement('p');
     branchHeader.textContent = host;
     contenedor.appendChild(branchHeader);
-
+    
     if (!event.target.classList.contains('selected')) {
         dispositivos_seleccionados.push(host)
         event.target.classList.add('selected');
@@ -48,25 +47,27 @@ function addHost(event) {
         
         console.log('add', host, dispositivos_seleccionados)
     }
+    console.log('addHost: ', id)
     // conectar(host)
 }
 function removeHost(event) {
     event.preventDefault();
     host = event.target
-
+    parent = host.parentElement
+    console.log('parent', parent)
+    
+    console.log('remove', dispositivos_seleccionados, host);
     if (host.classList.contains('selected')) {
         dispositivos_seleccionados.pop(host)
         host.classList.remove('selected');
-        consola_individual.removeChild(document.getElementById('div_'+host.id))
-        
+        parent.removeChild(document.getElementById(host))
     } else if (host.classList.contains('consola')){
         id = host.id.replace('div_', '')
         li = document.getElementById(id)
         li.classList.remove('selected');
         dispositivos_seleccionados.pop(id)
-        consola_individual.removeChild(document.getElementById('div_'+id))
+        parent.removeChild(document.getElementById('div_'+id))
     }
-    console.log('remove', dispositivos_seleccionados, host);
 }
 function addMultipleHost(event) {
     tipoDispositivo = event.target
